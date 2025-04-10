1c355f74b68e8ab35cfb6eead0c80709974cd019
Filippo <filippo.ianniello@gmail.com>
2025-04-10 15:39:30 +0100

    update setup

--- Keys_made_by_Filippo/pki_setup.sh
+++ Keys_made_by_Filippo/pki_setup.sh
@@ -103,7 +103,7 @@ chmod 444 ../intermediateCA/certs/full-chain.cert.pem
 
 # End of the 2° Phase: now ou Intermediate CA is ready to start issuing Subscriber Certificates.
 
-################## 3° Phase - Subscriber Certificate Setup ##################
+################## 3° Phase - Server Subscriber Certificate Setup ##################
 """
 The first step depends on the Subscriber who needs a signed certificate. They can ask you to either create
 the certificate for them (including the private key!), or they create one themselves and send a Certificate
@@ -123,7 +123,7 @@ chmod 400 ../server1/server1.key.pem
 # 3.2 - Create the server certificate signing request (CSR) using the private key
 # Note: For server certificates, the Common Name must be a fully qualified domain name (e.g.,
 # www.example.com).
-openssl req -config openssl_IntermediateCA.cnf -new -sha256 -key ../server1/server1.key.pem -out ../server1/server1.csr.pem
+openssl req -config openssl_IntermediateCA.cnf -key ../server1/server1.key.pem -new -sha256 -out ../server1/server1.csr.pem
 # We virtually send this request to the Intermediate CA, which will verify the request and sign it.
 cp ../server1/server1.csr.pem csr
 
@@ -141,3 +141,96 @@ openssl x509 -noout -text -in ../server1/server1.cert.pem
 # Note: in this case we are verifying all the chain because ca-chain.cert.pem contains both the Root CA and the Intermediate CA certificates
 # but in reality we will already have the Root CA certificate installed in the devices (e.g., web browsers, users), so we can just verify the Intermediate chain
 openssl verify -CAfile certs/full-chain.cert.pem ../server1/server1.cert.pem
+
+# 3.6 - Create the server certificate chain
+# NOTE: In order to not have problems with the alignment between the key and the certificate, we need to specify **in Order** the chain of the certificates
+# So in this case, the server1 certificate is the first one, then the intermediate CA certificate. The root is not needed because it will be installed in the devices
+cat ../server1/server1.cert.pem certs/intermediateCA.cert.pem > ../server1/server1-chain.cert.pem
+
+################## 4° Phase - Setting up the CRL (Certificate Revocation List) ##################
+# Before starting:
+# To make the clients check for the CRL, we need to update in the config file the section
+# server_crt with: crlDistributionPoints = URI:http://localhost/intermediateCA.crl when generating a certificate
+mkdir ../server_with_clr_check
+openssl genrsa -aes256 -out ../server_with_clr_check/server_with_clr_check.key.pem 2048
+openssl req -config openssl_IntermediateCA.cnf -key ../server_with_clr_check/server_with_clr_check.key.pem -new -sha256 -out ../server_with_clr_check/server_with_clr_check.csr.pem
+cp ../server_with_clr_check/server_with_clr_check.csr.pem csr
+# NOTE: in this case we use the extension server_cert_with_san_crl_ocsp because we need to add the CRL and OCSP information in the certificate
+openssl ca -config openssl_IntermediateCA.cnf -extensions server_cert_with_san_crl_ocsp -days 375 -notext -md sha256 -in csr/server_with_clr_check.csr.pem -out certs/server_with_clr_check.cert.pem
+cp certs/server_with_clr_check.cert.pem ../server_with_clr_check
+cat ../server_with_clr_check/server_with_clr_check.cert.pem certs/intermediateCA.cert.pem > ../server_with_clr_check/server_with_clr_check-chain.cert.pem
+openssl rsa -in ../server_with_clr_check/server_with_clr_check.key.pem -out ../server_with_clr_check/server_with_clr_check_decrypted.key.pem
+
+####### UNDERSTAND BETTER why it doesn't give an error even if the CRL is not available
+# Once done this, we can check if the certificate has the CRL distribution point by ispecting the certificate
+# and see if there is this section X509v3 CRL Distribution Points:
+openssl x509 -in ../server_with_clr_check/server_with_clr_check.cert.pem -noout -text
+
+# 4.1 Revoking a certificate
+cd ../intermediateCA
+openssl ca -config openssl_IntermediateCA.cnf -revoke certs/server_with_clr_check.cert.pem
+
+# 4.2 - Generate the CRL (it will generate it from the update of the database)
+openssl ca -config openssl_IntermediateCA.cnf -gencrl -out crl/intermediateCA.crl
+
+# 4.3 View the content of the CRL
+openssl crl -in crl/intermediate.crl -text -noout
+
+# Or we can start a web server to serve the CRL file.
+python3 -m http.server 8000 --directory crl/
+# Then we will need to contact http://localhost:8000/intermediateCA.crl
+# This should be the real address
+
+################## 5° Phase - Setting up OCSP (Online Certificate Status Protocol) ##################
+
+# As we did for the CRL, also for OCSP, we need to add in the config file the section 
+# authorityInfoAccess = OCSP;URI:http://localhost/ocsp
+# And then we need to sign another certificate with this config file that has this section
+# (there won't be reported again the steps to generate another certificate with this properties)
+# After we created the certificate with the information of the OCSP, we can check if the certificate has this section:
+openssl x509 -in ../server_with_ocsp_check/server_with_ocsp_check.cert.pem -noout -text
+
+# Once done the previous step, we can start configuring the OCSP server, by doing the following steps
+# 5.1 Creating a key for the OCSP server
+openssl genrsa -aes256 -out ../ocsp_server/ocsp_server.key.pem 4096
+
+# 5.2 Creating a CSR request in order to issue a OCSP certificate
+openssl req -config openssl_IntermediateCA.cnf -new -sha256 -key ../ocsp_server/ocsp_server.key.pem -out ../ocsp_server/ocsp_server.csr.pem
+cp ../ocsp_server/ocsp_server.csr.pem csr       # Moving the CSR request to the IntermediateCA
+
+# 5.3 Validating and issuing the certificate
+openssl ca -config openssl_IntermediateCA.cnf -extensions ocsp -days 375 -notext -md sha256 -in csr/ocsp_server.csr.pem -out certs/ocsp_server.cert.pem
+cp certs/ocsp_server.cert.pem ../ocsp_server
+
+# 5.4 Checking if in the certificate is reported that its usage is:
+# X509v3 Extended Key Usage: critical OCSP Signing
+openssl x509 -in ../ocsp_server/ocsp_server.cert.pem -noout -text
+
+# 5.5 - Create the OCSP responder certificate chain
+cat ../ocsp_server/ocsp_server.cert.pem certs/intermediateCA.cert.pem > ../ocsp_server/ocsp_server-chain.cert.pem
+
+# 5.6 - Running an OCSP responder
+# NOTE: The OCSP responder must be running on another server, so we need to copy the OCSP responder certificate and the private key to that OCSP server
+# -port Listen on port 2560 locally
+# -text Print request/response in human-readable form (for debugging)
+# -index The CA's index file — this tracks issued/revoked certs
+# -CA chain used to validate the OCSP signer
+# -rkey Private key of the OCSP responder certificate
+# -rsigner OCSP responder certificate
+# -nrequest Quit after serving 2 requests (useful for testing)
+openssl ocsp \
+    -port 2560 \
+    -text \
+    -index index.txt \
+    -CA ../ocsp_server/ocsp_server-chain.cert.pem \
+    -rkey ../ocsp_server/ocsp_server.key.pem \
+    -rsigner ../ocsp_server/ocsp_server.cert.pem \
+    -nrequest 2
+
+# 5.7 - Check the OCSP response with the OCSP responder
+openssl ocsp \
+  -issuer certs/intermediateCA.cert.pem \
+  -cert ../server1/server1.cert.pem \
+  -url http://localhost:2560 \
+  -header Host=localhost:2560 \
+  -no_nonce
